20e9d487091747dcff8f953c82992e205293fd1e
apiip0709 <afifalhaq777@gmail.com>
2023-10-17 19:40:42 +0800

    feat: detail for menuScript

--- js/menuScript.js
+++ js/menuScript.js
@@ -1,8 +1,10 @@
+// Untuk menampilkan popUp TopUp
 function showPopup() {
     document.getElementById('overlay').style.display = 'block';
     document.getElementById('popup').style.display = 'block';
 }
 
+// Untuk menyembunyikan popUp TopUp
 function hidePopup() {
     document.getElementById('overlay').style.display = 'none';
     document.getElementById('popup').style.display = 'none';
