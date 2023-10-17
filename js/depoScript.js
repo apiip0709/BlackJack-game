d9aabc5dfa9e76694f375f8435daa0ff429a3ec9
apiip0709 <afifalhaq777@gmail.com>
2023-10-17 19:42:28 +0800

    feat: detail for depoScript

--- js/depoScript.js
+++ js/depoScript.js
@@ -1,13 +1,17 @@
+// Menginisialisasikan ke dalam variabel data dari html
 const inputTopup = document.getElementById("input-topup");
 const btnSubmit = document.getElementById("submit");
 const resultTopUp = document.getElementById("result-topup")
 
+// Action button Submit
 btnSubmit.addEventListener("click", function () {
+    // Kondisi inputan selain angka dan kurang dari sama dengan 0
     if (isNaN(inputTopup.value) || inputTopup.value <= 0) {
         alert("Please enter a valid bet amount.");
         return;
     }
 
+    // Mengatur tambahan Your money sesuai inputan
     setTimeout(() => {
         const topupAmount = parseInt(inputTopup.value);
         const currentSums = parseInt(yourMoney.textContent);
